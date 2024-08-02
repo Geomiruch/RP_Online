69b0411348432958bf765320346d01184fb17639
Roman Leshchyshun <poliseroma@gmail.com>
2024-08-02 18:24:15 +0300

    Minor changes

--- RP_Server/RP_Server/Services/Implementation/CharacterService.cs
+++ RP_Server/RP_Server/Services/Implementation/CharacterService.cs
@@ -21,24 +21,18 @@ namespace RP_Server.Services.Implementation
             var result = new CharactersDto();
             result.Characters = _characterRepository.GetAll().Result.Select(_mapper.Map<CharacterDto>).ToList();
             return result;
+            return new CharactersDto().Characters = _characterRepository.GetAll().Result.Select(_mapper.Map<CharacterDto>).ToList();
         }
         public CharacterDto GetById(int id)
-        {
-            return _mapper.Map<CharacterDto>(_characterRepository.GetById(id));
-        }
+            => _mapper.Map<CharacterDto>(_characterRepository.GetById(id));
+
         public bool Delete(int id)
-        {
-            return _characterRepository.Delete(id);
-        }
+            => _characterRepository.Delete(id);
+
         public CharacterDto Create(CharacterCreateRequeest request)
-        {
-            var result = _characterRepository.Create(_mapper.Map<Character>(request));
-            return _mapper.Map<CharacterDto>(result);
-        }
+            => _mapper.Map<CharacterDto>(_characterRepository.Create(_mapper.Map<Character>(request)));
+
         public CharacterDto Update(CharacterDto request)
-        {
-            var result = _characterRepository.Update(_mapper.Map<Character>(request));
-            return _mapper.Map<CharacterDto>(result);
-        }
+            => _mapper.Map<CharacterDto>(_characterRepository.Update(_mapper.Map<Character>(request)));
     }
 }
